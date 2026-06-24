e6c629ce18f6022610ca4b785ba074c0466953ad
wizzygrace247 <wisemma2006@gmail.com>
2026-06-24 22:18:28 +0100

    modified Resume card

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -8,8 +8,14 @@ const ResumeCard = ({ resume }: { resume: Resume }) => {
         <Link to={`/resume/${resume.id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
                 <div className="flex flex-col gap-2">
-                    <h2 className="text-white font-bold break-words">{resume.companyName}</h2>
-                    <h3 className="text-lg break-words text-gray-500">{resume.jobTitle}</h3>
+                    {resume.companyName && (
+                        <h2 className="text-white font-bold break-words">{resume.companyName}</h2>
+                    )}
+                    {resume.jobTitle && (
+                        <h3 className="text-lg break-words text-gray-500">{resume.jobTitle}</h3>
+                    )}
+                    {!resume.companyName && !resume.jobTitle && <h2 className="!text-black font-bold">Resume</h2>}
+
                 </div>
                 <div className="flex-shrink-0">
                     <ScoreCircle score={resume.feedback.overallScore} />
@@ -18,13 +24,13 @@ const ResumeCard = ({ resume }: { resume: Resume }) => {
             <div className="w-full h-full">
 
                 <img
-             src={resume.imagePath}
-              alt="resume"
-             className="w-full h-[360px] max-sm:h-[200px] object-cover"
-                onError={(e) => {
-                e.currentTarget.src = "https://placehold.co/400x560?text=Resume";
-  }}
-/>
+                    src={resume.imagePath}
+                    alt="resume"
+                    className="w-full h-[360px] max-sm:h-[200px] object-cover"
+                    onError={(e) => {
+                        e.currentTarget.src = "https://placehold.co/400x560?text=Resume";
+                    }}
+                />
             </div>
         </Link>
     );
